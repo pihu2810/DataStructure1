1d64a04fc837a37a14e41851315bdcba2ddb3716
pihu2810 <vishaka.kadam01@gmail.com>
2022-04-06 18:13:18 +0530

    add LinkedList

---
+++ src/com/Bridglabz/LinkedListMain.java
@@ -0,0 +1,20 @@
+package com.Bridglabz;
+
+public class LinkedListMain 
+{
+  public static void main(String[] args) {
+	  LinkedList<Integer> list=new LinkedList<>();
+      list.add(23);
+      list.add(45);
+      list.add(19);
+      list.add(89);
+      list.insertAtLast(57);
+      list.insertAtPosition(2,11);
+      list.deleteFirst();
+      list.deleteLast();
+      list.deleteAtPosition(2);
+      list.printList();
+      
+	}
+  }
+
