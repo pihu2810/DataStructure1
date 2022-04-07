0a78f192d50ef6d6c7f45f84719164a6d30e230a
pihu2810 <101192637+pihu2810@users.noreply.github.com>
2022-04-07 14:27:47 +0530

    Update StackMain.java

--- src/com/Bridglabz/StackMain.java
+++ src/com/Bridglabz/StackMain.java
@@ -3,7 +3,7 @@ package com.Bridglabz;
 public class StackMain 
 {
 	 public static void main(String[] args) {
-		  Stack stack=new Stack();
+		  Stack<Integer> stack=new Stack<>();
 		  stack.push(37);
 		  stack.push(68);
 		  stack.push(19);
@@ -11,6 +11,8 @@ public class StackMain
 		  stack.push(75);
 		  stack.peak();
 		  stack.pop();
+		  stack.isEmpty();
+		  stack.getsize();
 		  stack.printStack();
 		  
 	  }
